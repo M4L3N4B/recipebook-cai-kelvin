90aef37433c70edbdd1c2fe6509e8a7440086f5c
M4L3N4B <caikelvin0315@gmail.com>
2025-02-24 19:24:38 +0800

    Set up URL access to HTML files

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
 
-# Create your views here.
+recipes = [
+    {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    },
+
+    {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+]
+
+def recipe_list(request):
+    return render(request, 'recipe_list.html', {'recipes': recipes})
+
+def recipe_preview(request, recipe_num):
+    if 1 <= recipe_num <= len(recipes):
+        recipe = recipes[recipe_num-1]
+        return render(request, 'recipe_preview.html', {'recipe': recipe})
