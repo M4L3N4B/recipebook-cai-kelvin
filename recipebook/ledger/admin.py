d741e2d1bf4c65ea41f17708a0cd71994e2d12c2
M4L3N4B <caikelvin0315@gmail.com>
2025-03-05 23:18:29 +0800

    Customized admins for Recipe, Ingredient, RecipeIngredient models

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+    search_fields = ('name', )
+    list_display = ('name', )
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    search_fields = ('name', )
+    list_display = ('name', )
+    
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
+    search_fields = ('recipe__name', 'ingredient__name', )
+    list_display = ('recipe', 'ingredient', 'quantity', )
+    list_filter = ('recipe', 'ingredient', )
+
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
