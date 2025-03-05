610b8b19d930755551b5cf0566b2f4e4191babc3
M4L3N4B <caikelvin0315@gmail.com>
2025-03-05 21:55:25 +0800

    Modified the detailed view to use Recipe

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('recipes/list/', views.recipe_list, name='recipe_list'),
-    path('recipe/<str:name>/', views.recipe_detail, name='recipe_detail'),
+    path('recipe/<str:recipe_name>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,76 +1,12 @@
 from django.shortcuts import render
 from .models import Recipe
 
-recipes = [
-    {
-        "name": "Recipe 1",
-        "ingredients": [
-            {
-                "name": "tomato",
-                "quantity": "3pcs"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "pork",
-                "quantity": "1kg"
-            },
-            {
-                "name": "water",
-                "quantity": "1L"
-            },
-            {
-                "name": "sinigang mix",
-                "quantity": "1 packet"
-            }
-        ],
-        "link": "/recipe/1"
-    },
-
-    {
-        "name": "Recipe 2",
-        "ingredients": [
-            {
-                "name": "garlic",
-                "quantity": "1 head"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "vinegar",
-                "quantity": "1/2cup"
-            },
-            {
-                "name": "water",
-                "quantity": "1 cup"
-            },
-            {
-                "name": "salt",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "whole black peppers",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "pork",
-                "quantity": "1 kilo"
-            }
-        ],
-        "link": "/recipe/2"
-    }
-]
-
 def recipe_list(request):
     recipes = Recipe.objects.all()
     ctx = {'recipes': recipes}
     return render(request, 'recipe_list.html', ctx)
 
-def recipe_detail(request, recipe_num):
-    if 1 <= recipe_num <= len(recipes):
-        recipe = recipes[recipe_num-1]
-        return render(request, 'recipe_detail.html', {'recipe': recipe})
+def recipe_detail(request, recipe_name):
+    recipe = Recipe.objects.get(name=recipe_name)
+    ctx = {'recipe': recipe}
+    return render(request, 'recipe_detail.html', ctx)
